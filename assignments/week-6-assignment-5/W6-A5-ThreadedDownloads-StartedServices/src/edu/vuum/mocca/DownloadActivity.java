0526b02e2c2a35cbc15b0fbd313eb8ef3b935d16
David Murillo <david.murillo@runtime.com.co>
2014-06-30 13:17:38 -0500

    solucion week 6

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -2,6 +2,8 @@ package edu.vuum.mocca;
 
 import java.lang.ref.WeakReference;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -78,6 +80,8 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+            	String path = (String) msg.getData().get(DownloadUtils.PATHNAME_KEY);
+            	activity.displayBitmap(path);
             }
     	}
     }
@@ -107,7 +111,8 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+        	Intent i = DownloadIntentService.makeIntent(this, handler, getUrlString());
+        	startService(i);
             which = "Starting DownloadIntentService";
             break;
         
@@ -115,7 +120,8 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
-
+        	i = ThreadPoolDownloadService.makeIntent(this, handler, getUrlString());
+        	startService(i);
             which = "Starting ThreadPoolDownloadService";
             break;
         
